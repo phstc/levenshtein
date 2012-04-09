45bb7b206f91487c7e30b9ccc54010037022e939
Pablo Cantero <pablo@pablocantero.com>
2012-04-09 18:45:11 -0300

    Adding friends totals

--- src/WordListReader.coffee
+++ src/WordListReader.coffee
@@ -16,7 +16,11 @@ fs.readFile "#{__dirname}/../assets/word.list.txt", 'utf-8', (err, data) ->
   if err
     console.error "Could not open file: %s", err
     process.exit 1
-  lines = data.split /\n/
-  for line, i in lines 
-    console.log line if "causes".levenshteinDistance(line) == 1
-    console.log (new Date().getTime() - startTime) / 1000 if i == (lines.length - 1)
+  friends = 0
+  data.split(/\n/).forEach (line, i, arr) ->
+    if "causes".levenshteinDistance(line) == 1 
+      console.log line 
+      friends++
+    if i == (arr.length - 1)
+      console.log "friends: #{friends}"
+      console.log "Time spent: #{(new Date().getTime() - startTime) / 1000}s"
--- src/WordListReader.js
+++ src/WordListReader.js
@@ -1,5 +1,5 @@
 (function() {
-  var fs, includeInThisContext, vm;
+  var fs, includeInThisContext, startTime, vm;
 
   fs = require('fs');
 
@@ -11,16 +11,22 @@
     return vm.runInThisContext(code, path);
   };
 
-  includeInThisContext.bind(this);
-
   includeInThisContext("" + __dirname + "/Levenshtein.js");
+
+  startTime = new Date().getTime();
 
   fs.readFile("" + __dirname + "/../assets/word.list.txt", 'utf-8', function(err, data) {
     if (err) {
       console.error("Could not open file: %s", err);
       process.exit(1);
     }
-    return console.log("causes".levenshteinDistance("data"));
+    console.log("aaa");
+    return data.split(/\n/).forEach(function(line) {
+      if ("causes".levenshteinDistance(line) === 1) console.log(line);
+      if (i === (lines.length - 1)) {
+        return console.log((new Date().getTime() - startTime) / 1000);
+      }
+    });
   });
 
 }).call(this);
